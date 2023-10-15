847eecc9d613c8614e3c642afbd843d932e784a6
Rafid-Al-Ahsan <rafidahsan97@gmail.com>
2023-10-15 15:24:03 +0600

    banner added in home page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,13 +1,67 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import Header from './shared/Header';
-import Footer from './shared/Footer/Footer';
+
 
 const Home = () => {
+
+
+    const containerStyle = {
+        position: 'relative',
+        width: '100%',
+        height: '100%',
+    };
+
+    const backgroundStyle = {
+        backgroundImage: 'url("https://demos.hogash.com/phaeton-restaurant-html/images/slide1.jpg")',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover', 
+        width: '100%',
+        height: '40rem',
+      
+      }
+      
+      const overlayStyle = {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        background: 'rgba(0, 0, 0, 0.5)', 
+      };
+
+      const textStyle = {
+        position: 'absolute',
+        top: '50%', 
+        left: '50%', 
+        transform: 'translate(-50%, -50%)', 
+        color: 'white', 
+        fontSize: '4rem', 
+      };
+    
+
     return (
+
         <div>
-           This is home page
+            <div style={containerStyle}>
+                <div style={backgroundStyle}></div>
+                <div style={overlayStyle}></div>
+                <div style={textStyle}>
+                    <p>Delicious Italian Cuisines</p>
+                    <div className='d-flex justify-content-center'>
+                        <button className="btn py-3 px-5" style={{backgroundColor: "black", color: 'white', }}>SEE OUR MENU</button>
+                    </div>
+                </div>
+            </div>      
+
+            
+
+     
+
+
         </div>
+
+
     );
 };
 
